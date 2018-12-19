fb4799d03b386dd6183c770328d3ad5b74899f43
exodiarap1 <45983226+exodiarap1@users.noreply.github.com>
2018-12-19 07:35:32 +0200

    Add files via upload

---
+++ link.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+ client.on('message', message => {
+    if (message.content.startsWith("هات لينك ي ايكسوديا")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
+
+client.login("NTI0MzY2MzEwODE0MjUzMDc2.DvnCKQ.ao3vd1k9nAi_-JlGQFJBK7ZrwIA")
